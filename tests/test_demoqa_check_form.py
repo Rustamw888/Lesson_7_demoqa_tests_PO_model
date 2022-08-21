d9cfb02b982d9e66b4faee06ff515e52e676d7ca
qaMilord <rustamw888@gmail.com>
2022-08-21 03:11:50 +0300

    homework done

--- tests/test_demoqa_check_form.py
+++ tests/test_demoqa_check_form.py
@@ -26,21 +26,22 @@ def test_submit_form():
     application_manager.opened_practice_form()
 
     # WHEN
-    application_manager.form\
-        .set_first_name(student.first_name) \
-        .set_last_name(student.last_name) \
-        .set_email(student.email) \
-        .set_gender(student.gender) \
-        .set_mobile_number(student.mobile_number) \
-        .set_birth_date(student.birth_year, student.birth_month, student.birth_day) \
-        .set_subjects(student.subjects) \
-        .subjects_should_have(student.subjects) \
-        .set_hobbies(student.hobbies) \
-        .set_photo(student.photo) \
-        .set_current_address(student.current_address) \
-        .set_state(student.state) \
-        .set_city(student.city) \
+    (application_manager.form
+        .set_first_name(student.first_name)
+        .set_last_name(student.last_name)
+        .set_email(student.email)
+        .set_gender(student.gender)
+        .set_mobile_number(student.mobile_number)
+        .set_birth_date(student.birth_year, student.birth_month, student.birth_day)
+        .set_subjects(student.subjects)
+        .subjects_should_have(student.subjects)
+        .set_hobbies(student.hobbies)
+        .set_photo(student.photo)
+        .set_current_address(student.current_address)
+        .set_state(student.state)
+        .set_city(student.city)
         .submit()
+    )
 
     # THEN
     application_manager.result.verify_sent_data(
